877202114369164546f4aa6a5e3eee18a33a871b
KirilSq <kiril.cv.kirilov@gmail.com>
2026-02-04 23:24:30 +0200

    added firebase sign in/up

--- js/login.js
+++ js/login.js
@@ -1,7 +1,69 @@
-let signInSelectButton = document.getElementById("signin-select-btn");
-let signUpSelectButton = document.getElementById("signup-select-btn");
-let signUpElements = document.getElementsByClassName("signup-el");
-let signInElements = document.getElementsByClassName("signin-el");
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlAIqRDsb7bM_BgogJGMikHQlC-mndwOk",
+  authDomain: "project-add-ap.firebaseapp.com",
+  projectId: "project-add-ap",
+  storageBucket: "project-add-ap.firebasestorage.app",
+  messagingSenderId: "278432857215",
+  appId: "1:278432857215:web:89e7bffb14e2d9af50934a",
+  measurementId: "G-8ZRG7HCHH5",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const signInSelectButton = document.getElementById("signin-select-btn");
+const signUpSelectButton = document.getElementById("signup-select-btn");
+const signUpElements = document.getElementsByClassName("signup-el");
+const signInElements = document.getElementsByClassName("signin-el");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+const signInBtn = document.querySelector(".submit-btn.signin-el");
+const signUpBtn = document.querySelector(".submit-btn.signup-el");
+
+if (signUpBtn) {
+  signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Registration error:", errorCode, errorMessage);
+        alert("Registration failed:" + errorMessage);
+      });
+  });
+}
+
+if (signInBtn) {
+  signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Sign-in error:", errorCode, errorMessage);
+        alert("Sign-in failed:" + errorMessage);
+      });
+  });
+}
 
 function updateDisplayFromSignMode() {
   Array.from(signUpElements).forEach((element) => {
