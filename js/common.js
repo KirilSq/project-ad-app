27a8529c180f42b7f1c576e405038e94ffe2f146
KirilSq <kiril.cv.kirilov@gmail.com>
2026-02-05 11:31:28 +0200

    added database manipulation for removing/adding ads from favorites/cart

--- js/common.js
+++ js/common.js
@@ -15,20 +15,56 @@ const firebaseConfig = {
   messagingSenderId: "278432857215",
   appId: "1:278432857215:web:89e7bffb14e2d9af50934a",
   measurementId: "G-8ZRG7HCHH5",
-  databaseURL: https://project-add-ap-default-rtdb.europe-west1.firebasedatabase.app/
+  databaseURL:
+    "https://project-add-ap-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-
-async function removeFromFavorites(userId, adId){
-    try {
-      await remove(ref(database , `users/${userId}/favorites/${adId}`));
-    } catch(error){
-        console.error("Error removing from favorites: ", error);
-    }
-    
+async function removeFromFavorites(userId, adId) {
+  try {
+    await remove(ref(database, `users/${userId}/favorites/${adId}`));
+  } catch (error) {
+    console.error("Error removing from favorites: ", error);
+  }
 }
 
+async function addToFavorites(userId, adId) {
+  try {
+    await set(ref(database, `users/${userId}/favorites/${adId}`), true);
+  } catch (error) {
+    console.error("Error adding to favorites: ", error);
+  }
+}
+
+async function addToCart(userId, adId) {
+  try {
+    await set(ref(database, `users/${userId}/cart/${adId}`), true);
+    return true;
+  } catch (error) {
+    console.error("Error adding to cart: ", error);
+    return false;
+  }
+}
+
+async function removeFromCart(userId, adId) {
+  try {
+    await remove(ref(database, `users/${userId}/cart/${adId}`));
+    return true;
+  } catch (error) {
+    console.error("Error removing from cart: ", error);
+    return false;
+  }
+}
+
+async function updateUserBalance(userId, newBalance) {
+  try {
+    await set(ref(database, `users/${userId}/balance`), newBalance);
+    return true;
+  } catch (error) {
+    console.error("Error updating balance: ", error);
+    return false;
+  }
+}
